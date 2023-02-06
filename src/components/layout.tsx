9ba73565e902f3ba564793e918054a774ef66743
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-02-06 13:43:12 +0100

    Created ProgressBar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,5 @@
+import ProgressBar from "./progressBar";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -5,6 +7,7 @@ interface LayoutProps {
 function Layout({ children }: LayoutProps) {
   return (
     <>
+      <ProgressBar />
       <main>{children}</main>
     </>
   );
