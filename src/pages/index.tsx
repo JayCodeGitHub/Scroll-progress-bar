85d28e829f3d86d8f5b0a5a01a39ba6ae491b32e
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-02-06 13:31:46 +0100

    Setup Tailwind CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Scroll progress bar</h1>
+        <h1 className="text-red-500">Scroll progress bar</h1>
       </main>
     </>
   );
